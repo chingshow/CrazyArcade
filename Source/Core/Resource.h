1197ce3ebd11bb08f45075df892728b29e5e3120
Hinarin2017 <b812110011@tmu.edu.tw>
2024-03-15 09:24:23 +0800

    resourse added

--- Source/Core/Resource.h
+++ Source/Core/Resource.h
@@ -3,6 +3,7 @@
 // Used by game.rc
 //
 #define IDD_ABOUTBOX                    100
+#define IDC_GAMECURSOR                  153
 #define IDP_SOCKETS_INIT_FAILED         104
 #define IDR_MAINFRAME                   128
 #define IDR_GAMETYPE                    129
@@ -17,6 +18,122 @@
 #define ID_HELP_ABOUTFRAMEWORK          32776
 #define ID_Menu                         32777
 #define ID_APP_FRAMEWORK                57682
+
+#define BNB                             997
+#define icon32                          998   
+#define icon256                         999   
+
+
+#define map1                            140  
+#define blocks_r                        141     
+#define blocks_y                        142
+#define grass                           143
+#define house_b                         144
+#define house_r                         145
+#define house_y                         146
+#define tree                            147
+#define wooden_box                      148   
+
+
+#define map2                            200   
+#define cactus                          201     
+#define earth                           202 
+#define green_box                       203                                   
+#define green_box2                      204        
+#define rock                            205
+#define tent_b                          206                               
+#define tent_r                          207    
+#define tent_y                          208  
+
+
+
+#define map3                            300   
+#define basket_trans                    301
+#define flower_trans                    302    
+#define grassBlock_trans                303             
+#define tree_trans                      304
+#define pool_LB_trans                   310
+#define pool_LU_trans                   311          
+#define pool_MB_trans                   312
+#define pool_MU_trans                   313
+#define pool_RB_trans                   314          
+#define pool_RU_trans                   315
+
+
+
+#define map4                            400
+#define box                             401
+#define rock1                           402
+#define rock2                           403
+#define stone                           404  
+#define cross_trans_B                   405          
+#define cross_trans_L                   406          
+#define cross_trans_R                   407          
+#define iron1_trans_B                   408          
+#define iron2_trans_B                   409          
+#define lamb_trans_B                    410
+
+
+
+#define map5                            500 
+#define block                           501  
+#define block2_trans                    502         
+#define bottle_d                        510     
+#define bottle_u                        511
+#define bottle_B                        503     
+#define box_b                           504  
+#define box_d                           505  
+#define triangle_trans                  506
+
+
+                            
+#define Player1_down                    600          
+#define Player1_left                    601          
+#define Player1_right                   602          
+#define Player1_up                      603 
+#define Player1_die1                    604          
+#define Player1_die2                    605  
+
+
+#define Player2_down                    700          
+#define Player2_left                    701          
+#define Player2_right                   702          
+#define Player2_up                      703 
+#define Player2_die1                    704          
+#define Player2_die2                    705  
+
+
+#define home_page                       900
+#define map_select                      901     
+#define game_end1                       902   
+#define game_end2                       903
+#define loading                         950    
+#define IDB_continue                    951         
+#define center                          952  
+#define corner                          953   
+                          
+#define explode                         800       
+#define water_column                    801         
+#define water_column2                   802           
+#define water_down                      803       
+#define water_down_end                  804           
+#define water_left                      805       
+#define water_left_end                  806 
+#define water_right                     807        
+#define water_right_end                 808            
+#define water_up                        809     
+#define water_up_end                    810
+
+
+#define bomb                            001  
+#define bomb2                           002   
+#define bomb3                           003  
+#define needle                          004   
+#define props_1                         005     
+#define props_2                         006    
+#define props_3                         007
+#define null                            000
+
 
 // Next default values for new objects
 // 
